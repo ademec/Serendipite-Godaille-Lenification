23ab8b58f41e12abed5e259b4f7d263d34bc5eea
TimotheAlbouy <38692015+TimotheAlbouy@users.noreply.github.com>
2021-09-25 17:41:27 +0200

    generated surname lastname models

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from name_gen.name import gen_family_name, gen_surname
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+	return render_template("index.html")
+
+@app.route("/surname")
+def surname():
+	return 
+
+if __name__ == "__main__":
+	app.run()
---
+++ name_gen/name.py
@@ -0,0 +1,6 @@
+
+def gen_family_name():
+  pass
+
+def gen_surname():
+  pass
